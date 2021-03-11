e25cef46eb75cadd2b9eb18c9ff59b440e64120d
Kiara Ferguson <Kiaraferguson16@yahoo.com>
2021-03-11 16:43:19 -0500

    added seed file

--- config/config.js
+++ config/config.js
@@ -1,24 +1,23 @@
+/* eslint-disable no-undef */
 require("dotenv").config(); // this is important!
-module.exports = {
-  development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE,
-    host: process.env.DB_HOST,
-    dialect: "mysql"
-  },
-  test: {
-    username: "root",
-    password: null,
-    database: "database_test",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  },
-  production: {
-    username: "root",
-    password: null,
-    database: "database_production",
-    host: "127.0.0.1",
-    dialect: "mysql"
-  }
+export const development = {
+  username: process.env.DB_USERNAME,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_DATABASE,
+  host: process.env.DB_HOST,
+  dialect: "mysql"
+};
+export const test = {
+  username: "root",
+  password: null,
+  database: "database_test",
+  host: "127.0.0.1",
+  dialect: "mysql"
+};
+export const production = {
+  username: "root",
+  password: null,
+  database: "database_production",
+  host: "127.0.0.1",
+  dialect: "mysql"
 };
--- models/donuts.js
+++ models/donuts.js
@@ -1,4 +1,4 @@
-module.exports = function(sequelize, DataTypes) {
+export default function(sequelize, DataTypes) {
   var Donut = sequelize.define("Donut", {
       name: {
           type: DataTypes.STRING,
