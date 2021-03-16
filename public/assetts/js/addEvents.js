beec6a29e934b0935a91965d143ff50ea41c9c5f
Vanessa Patterson <vpatt1031@gmail.com>
2021-03-15 21:28:10 -0400

    fixed fields in the card

--- public/assetts/js/addEvents.js
+++ public/assetts/js/addEvents.js
@@ -9,18 +9,23 @@ fetch("/api/new", {
   .then((response) => response.json())
   .then((data) => {
     console.log("Success in grabbing event:", data);
-    data.forEach(({ title, category, date, description, vendors }, i) => {
+    data.forEach(({ title, category, date, description, team_leader, vendors }, i) => {
       console.log(title);
       console.log(category);
       console.log(date);
       console.log(description);
+      console.log(team_leader);
       console.log(vendors);
       console.log(i);
       
       $("#well-section").append(` <div class="col s2 m4"> <div class="card blue-grey darken-1">
           <div class="card-content white-text">
             <span class="card-title" id="cardTitle">${title}</span>
-            <p id="cardContent">Dynamically created short description/category name</p>
+            <p id="cardContent">${category}</p>
+            <p id="cardContent">${date}</p>
+            <p id="cardContent">${description}</p>
+            <p id="cardContent">${team_leader}</p>
+            <p id="cardContent">${vendors}</p>
           </div>
           <div class="card-action">
             <a href="/edit">Edit</a>
