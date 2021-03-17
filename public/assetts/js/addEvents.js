008fffefeb390490ddec45d2bb36810a38575e51
Vanessa Patterson <vpatt1031@gmail.com>
2021-03-17 11:46:08 -0400

    changed vendor selector to render it to the page

--- public/assetts/js/addEvents.js
+++ public/assetts/js/addEvents.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 // Code here will dictate how the page displays all the events
 
 fetch("/api/new", {
@@ -21,11 +22,13 @@ fetch("/api/new", {
       $("#well-section").append(` <div class="col s2 m4"> <div class="card card-style">
           <div class="card-content blue-grey lighten-5 blue-grey-text text-darken-4">
             <span class="card-title" id="cardTitle">${title}</span>
-            <p id="cardContent">${category}</p>
-            <p id="cardContent">${date}</p>
-            <p id="cardContent">${description}</p>
-            <p id="cardContent">${team_leader}</p>
-            <p id="cardContent">${vendors}</p>
+            <ul>
+            <li id="cardContent">Event Type: ${category}</li>
+            <li id="cardContent">Date: ${date}</li>
+            <li id="cardContent">Description: ${description}</li>
+            <li id="cardContent">Team Leader: ${team_leader}</li>
+            <li id="cardContent">Vendor: ${vendors}</li>
+            </ul>
           </div>
           <div class="card-action">
             <a class="blue-text text-darken-4" href="/edit">Edit</a>
--- server.js
+++ server.js
@@ -14,7 +14,7 @@ require("./routes/apiRoutes")(app)
 require("./routes/htmlRoutes")(app)
 
 
-db.sequelize.sync({force: true}).then(() => {
+db.sequelize.sync().then(() => {
     app.listen(PORT, function(){
         console.log("Server working")
     })
